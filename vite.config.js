cf2555f338b0ba3525db70f4add6b99f64b8ebf9
Eric_PC_FIXE <edelhaye93@gmail.com>
2024-07-17 20:02:55 +0200

    installing git pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Rock_Paper_Scissors_Eric/"
+  base: "/ShiFuMi/"
 })
